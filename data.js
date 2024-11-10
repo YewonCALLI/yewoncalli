4a264017b656194950bf7819eb33da662e7a9213
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2024-11-11 01:54:58 +0900

    1111 update-2

--- data.js
+++ data.js
@@ -20,7 +20,7 @@ const projects = [
     description:
       "A digital art project transforming user-inputted text into Korean signboard images.",
     projectLink: "ganpan.vercel.app",
-    image: "ganpan.png",
+    image: "hongyeon.png",
     link : 'works/ganpan.html'
   },
   {
@@ -36,41 +36,102 @@ const projects = [
     link : 'works/daduck.html'
   },
   {
-    title: "DA-DUCK",
+    title: "Word Wide Web",
     date: "2024",
-    tags: ["Code", "Branding Design"],
+    tags: ["Code", "Game"],
     projectType: "Team Project",
     event: "The Hyundai Seoul, B1 Event Plaza",
     description:
-      "p5.js character goods inspired by Yeongdeungpo’s mallard, sold at The Hyundai Department Store.",
-    projectLink: "daduck.vercel.app",
-    image: "daduck.png",
+      "A collaborative word-weaving game exploring poetic connections through custom similarity criteria",
+    projectLink: "https://solarword.vercel.app/",
+    image: "wordwideweb.png",
     link : 'works/daduck.html'
-  },{
-    title: "DA-DUCK",
+  },
+  {
+    title: "Memeproject",
+    date: "2024",
+    tags: ["Game", "Story", "Code"],
+    projectType: "Team Project",
+    event: "PlanT House, Hongcheon Art Museum",
+    description:
+      "An interactive website exploring meme origins, impact, and replication",
+    projectLink: "daduck.vercel.app",
+    image: "memeproject.png",
+    link : 'works/daduck.html'
+  },
+  {
+    title: "Hongyeon 1.0",
+    date: "2024",
+    tags: ["AI", "Story"],
+    projectType: "Team Project",
+    event: "The Silver Bell Challenge: Hong Yeon-gil Project, ten to the n",
+    description:
+      "A chatbot blending multigenerational voices from Hongyeon-gil, sharing local stories and culture through AI-driven narrative",
+    projectLink: "daduck.vercel.app",
+    image: "hongyeon.png",
+    link : 'works/daduck.html'
+  },
+  {
+    title: "ABT (Art, Body, Tech)",
     date: "2024",
     tags: ["Code", "Branding Design"],
     projectType: "Team Project",
-    event: "The Hyundai Seoul, B1 Event Plaza",
+    event: "The Silver Bell Challenge: Hong Yeon-gil Project, ten to the n",
     description:
-      "p5.js character goods inspired by Yeongdeungpo’s mallard, sold at The Hyundai Department Store.",
+      "Branding design for Team ABT’s project using p5.js code art to create event typography, posters, and a website",
     projectLink: "daduck.vercel.app",
-    image: "daduck.png",
+    image: "abt.png",
+    link : 'works/daduck.html'
+  },
+  {
+    title: "Simulating #1,2,3",
+    date: "2024",
+    tags: ["Computer Graphics", "Media Facade"],
+    projectType: "Team Project",
+    event: "ACC Media Cube, Gwangju",
+    description:
+      "Simulating gravity through Unreal Engine’s Blueprint",
+    projectLink: "daduck.vercel.app",
+    image: "simulating.png",
+    link : 'works/daduck.html'
+  },
+  {
+    title: "Singlet&Multiplet",
+    date: "2021",
+    tags: ["Code", "Media Facade"],
+    projectType: "Team Project",
+    event: "8th International New Media Art Exhibition, CICA Museum <br>Beyond the Lens : Nano Bio Nature, Seoul Institute of the Arts, IBS(Institute for Basic Science)",
+    description:
+      "A media art performance exploring identity and quantum mechanics through layered video art and live performance",
+    projectLink: "daduck.vercel.app",
+    image: "singlet&multiplet.png",
+    link : 'works/daduck.html'
+  },
+  {
+    title: "Draw the Beat!",
+    date: "2021",
+    tags: ["Code", "Game"],
+    projectType: "Team Project",
+    event: "8th International New Media Art Exhibition, CICA Museum <br>Beyond the Lens : Nano Bio Nature, Seoul Institute of the Arts, IBS(Institute for Basic Science)",
+    description:
+      "A program that creates music by drawing",
+    projectLink: "daduck.vercel.app",
+    image: "drawthebeat.png",
     link : 'works/daduck.html'
   },
 ];
 
-const research = [
-  {
-    title: "Franklin",
-    date: "2024",
-    tags: ["AI Debiasing", "Prompt Engineering"],
-    projectType: "Team Project",
-    event: "Performed : SFPC(School for Poetic Computation)",
-    description: `
-      Word Wide Web is a project I developed while attending the "Human-Scale Natural Language Processing" course at SFPC Summer School. During this course, around a dozen participants, including myself, contributed by curating and submitting our own unique text data. I submitted a poetic essay inspired by the works of Dai Yoko and George Eliot.
-      `,
-    projectLink: "",
-    image: "typefold.png",
-  },
-];
+// const research = [
+//   {
+//     title: "Franklin",
+//     date: "2024",
+//     tags: ["AI Debiasing", "Prompt Engineering"],
+//     projectType: "Team Project",
+//     event: "Performed : SFPC(School for Poetic Computation)",
+//     description: `
+//       Word Wide Web is a project I developed while attending the "Human-Scale Natural Language Processing" course at SFPC Summer School. During this course, around a dozen participants, including myself, contributed by curating and submitting our own unique text data. I submitted a poetic essay inspired by the works of Dai Yoko and George Eliot.
+//       `,
+//     projectLink: "",
+//     image: "typefold.png",
+//   },
+// ];
