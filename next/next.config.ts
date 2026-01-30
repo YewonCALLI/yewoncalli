74367d8d629ad59f9e56e3bb4a9a5dd0baebe64c
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2026-01-31 00:08:42 +0900

    add 2026

--- next/next.config.ts
+++ next/next.config.ts
@@ -18,6 +18,7 @@ const nextConfig = {
   // },
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   images: {},
+  transpilePackages: ['three'],  
   webpack(config, { isServer }) {
     if (!isServer) {
       // We're in the browser build, so we can safely exclude the sharp module
